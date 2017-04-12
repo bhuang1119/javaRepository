b1dc4d004729c427619da22d660f950eb2c38503
bhuang1119 <1451568552@qq.com>
2017-04-12 11:04:20 +0800

    test3

--- src/testGit/testGit.java
+++ src/testGit/testGit.java
@@ -5,7 +5,8 @@ public class testGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("------------");
-		
+		System.out.println("-----hb-------");
+
 		
 	}
 
